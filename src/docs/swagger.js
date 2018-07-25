7ce9cca9bd78c06ae349e7f088db2adcd4211f95
tobidev <tobi@gokada.ng>
2018-07-25 17:19:35 +0100

    add documentation for patch route

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,6 +1,8 @@
 const swagger = require('./swagger.json');
 const login = require('./user/login.json');
+const patch = require('./patch/jsonpatch.json');
 
 swagger.paths['/user/login'] = login;
+swagger.paths['/patch'] = patch;
 
 module.exports = swagger;
